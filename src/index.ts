f69203af511e7ab13bfea83fdefdca728744c1a7
sergioescobarh <sergio.escobar.henao@gmail.com>
2023-05-15 19:35:52 -0500

    initial structure

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port, ()=>{ console.log('listening on port ' + port ); });
+
+app.get('/api', (req, res) => {
+    res.send('Hi!!!')
+});
